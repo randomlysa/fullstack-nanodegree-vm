3106781b78bd5bdfa0a2a4e6ada730743dfe99c5
randomlysa <su@randomlysa.com>
2015-11-01 00:15:34 -0500

    doing things

--- vagrant/tournament/tournament_test.py
+++ vagrant/tournament/tournament_test.py
@@ -88,8 +88,8 @@ def testReportMatches():
     registerPlayer("Diane Grant")
     standings = playerStandings()
     [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2)
-    reportMatch(id3, id4)
+    reportMatch(id1, 'w', id2, 'l')
+    reportMatch(id3, 'w', id4, 'l')
     standings = playerStandings()
     for (i, n, w, m) in standings:
         if m != 1:
@@ -110,8 +110,8 @@ def testPairings():
     registerPlayer("Pinkie Pie")
     standings = playerStandings()
     [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2)
-    reportMatch(id3, id4)
+    reportMatch(id1, 'w', id2, 'l')
+    reportMatch(id3, 'w', id4, 'l')
     pairings = swissPairings()
     if len(pairings) != 2:
         raise ValueError(
@@ -135,5 +135,3 @@ if __name__ == '__main__':
     testReportMatches()
     testPairings()
     print "Success!  All tests pass!"
-
-
