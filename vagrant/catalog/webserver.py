5d8751d578ae19c0e488d3b52db517d847a38b7e
randomlysa <su@randomlysa.com>
2015-11-09 21:07:47 -0600

    added 'edit'

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -14,6 +14,7 @@ class webServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
 
         newPath = self.path.partition('?')
+        # main page
         if self.path.endswith("/restaurants"):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
@@ -24,7 +25,8 @@ class webServerHandler(BaseHTTPRequestHandler):
             restaurants =  session.query(Restaurant).all()
             for restaurant in restaurants:
                 message += "<h2>%s</h2>\n" % restaurant.name
-                message += "<a href=''>Edit</a> | <a href='/restaurants/delete?r=%s'>Delete</a>\n" % restaurant.id
+                message += "<a href='/restaurants/edit?r=%s'>Edit</a>\n" % restaurant.id
+                message += "<a href='/restaurants/delete?r=%s'>Delete</a>\n" % restaurant.id
                 # message += restaurant.name[0:-2]
             message += "<hr><a href='/restaurants/new'>Create a new restaurant</a>"
             message += "</body></html>\n\n"
@@ -32,6 +34,7 @@ class webServerHandler(BaseHTTPRequestHandler):
             print message
             return
 
+        # create new
         if self.path.endswith("/restaurants/new"):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
@@ -49,6 +52,34 @@ class webServerHandler(BaseHTTPRequestHandler):
             print output
             return
 
+        # edit
+        if newPath[0] == ("/restaurants/edit") or self.path == ("/restaurants/edit"):
+            editWhich = newPath[2].partition('=') # get the part after the ?, ie, the restarant id
+            editId = editWhich[2]
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            # print editId
+            editName = session.query(Restaurant.name).filter_by(id = editId).one()
+            # print editName
+
+            output = ""
+            output += " <html><body>\n"
+            output += " <h1>edit %s</h1>\n" % editName.name
+            output += " <form method='POST' enctype='multipart/form-data' action='/restaurants/edit'> \n\
+                        <input name='action' type='hidden' value='edit'> \n\
+                        <input name='restaurantid' type='hidden' value='%s'>\n\
+                        <input name='message' type='text'> \n\
+                        <input type='submit' value='edit it!'> \n\
+                        </form>\n" % editId
+            output += " </body></html>\n"
+            self.wfile.write(output)
+            print output
+            return
+
+            
+        # delete
         if newPath[0] == ("/restaurants/delete") or self.path == ("/restaurants/delete"):
             deleteWhich = newPath[2].partition('=') # get the part after the ?, ie, the restarant id
             deleteId = deleteWhich[2]
@@ -65,7 +96,7 @@ class webServerHandler(BaseHTTPRequestHandler):
             output += " <h1>Delete %s</h1>\n" % thisName.name
             output += " <form method='POST' enctype='multipart/form-data' action='/restaurants/delete'> \n\
                         <input name='action' type='hidden' value='delete'> \n\
-                        <input name='message' type='hidden' value='%s'><input type='submit' value='Delete it!'> \n\
+                        <input name='restaurantid' type='hidden' value='%s'><input type='submit' value='Delete it!'> \n\
                         </form>\n" % deleteId
             output += " </body></html>\n"
             self.wfile.write(output)
@@ -83,16 +114,22 @@ class webServerHandler(BaseHTTPRequestHandler):
             ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
             if ctype == 'multipart/form-data':
                 fields=cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
                 action = fields.get('action')[0]
+                if fields.get('message') != None:                    
+                    message = fields.get('message')[0]
+                if fields.get('restaurantid') != None:                    
+                    restaurantid = fields.get('restaurantid')[0]
 
-            print "action " 
-            print action
-            print "messagecontent[0] "
-            print messagecontent[0]
+            print fields
+            '''
+            print "action: ", action            
+            print "message: ", message
+            print "restaurantid: ", restaurantid
+            '''
 
+            # create
             if action == 'create':
-                addRestaurant = Restaurant(name = messagecontent[0])
+                addRestaurant = Restaurant(name = message)
                 session.add(addRestaurant)
                 session.commit()
 
@@ -102,7 +139,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += " <html><body>\n"
                 output += " <h2> Restaurant added: </h2>\n"
-                output += " <h1> %s </h1>\n" % messagecontent[0]
+                output += " <h1> %s </h1>\n" % message
 
                 output += " <form method='POST' enctype='multipart/form-data' action='/restaurants/new'> \n\
                             <h2> Add another one? </h2> \n\
@@ -114,10 +151,28 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print output
 
+            # edit
+            if action == 'edit':
+                
+                edit = session.query(Restaurant).filter_by(id = restaurantid).one()
+                edit.name = message
+                session.add(edit)
+                session.commit()
+                                
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""                
+                output += "Attempting to edit a restaurant"
+
+                self.wfile.write(output)
+                print output
+                
+            # delete
             if action == 'delete':
-                #deleteRestaurant = Restaurant(id = messagecontent[0])
-                #print deleteRestaurant
-                session.query(Restaurant).filter(Restaurant.id==messagecontent[0]).delete()
+                
+                delete = session.query(Restaurant).filter_by(id = restaurantid).one()
+                session.delete(delete)
                 session.commit()
                                 
                 self.send_response(200)
