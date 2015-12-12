9f52bc452eb78989543118a849a43224b1fcc97c
randomlysa <su@randomlysa.com>
2015-12-12 16:10:45 -0600

    catalog

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,33 +1,62 @@
-import os
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-
+ 
 Base = declarative_base()
 
-
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
-
+class User(Base):
+    __tablename__ = 'user'
+    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
-
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
-
-    name = Column(String(80), nullable=False)
+class Catalog(Base):
+    __tablename__ = 'catalog'
+   
     id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    header_image = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+       }
+ 
+class CatalogItem(Base):
+    __tablename__ = 'catalog_item'
+
+
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
     description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
+    image = Column(String(250))
+    catalog_id = Column(Integer,ForeignKey('catalog.id'))
+    catalog = relationship(Catalog)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'description'         : self.description,
+           'id'         : self.id,
+           'image'         : self.image,           
+       }
 
+
+
+engine = create_engine('sqlite:///catalogwithusers.db')
+ 
 
 Base.metadata.create_all(engine)
